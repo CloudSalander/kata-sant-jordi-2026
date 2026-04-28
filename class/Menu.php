a53d1b1a153db1875a24f658a8c97248e05add4e
CloudSalander <ruben.as532@gmail.com>
2026-04-28 18:43:03 +0200

    Now with Strategy Pattern! :)

--- class/Menu.php
+++ class/Menu.php
@@ -1,21 +1,18 @@
 <?php 
 include('Rose.php');
 include('Book.php');
-//TODO: Validate user entry
+
 class Menu {
     const INPUT_MESSAGE = "Please, choose option 1 or 2";
     const OPTIONS=["1- Rose", "2- Book"];
 
-    public function showOptions() {
-        $inputValue = readline(self::INPUT_MESSAGE);
+    public function selectOption(): string {
         foreach(self::OPTIONS as $option) echo $option.PHP_EOL;
-        $this->takeGift($inputValue);
+        $inputValue = readline(self::INPUT_MESSAGE);
+        return $inputValue;
     }
 
-    public function takeGift(int $gift): void {
-        if($gift == 1) new Rose()->takeGift();
-        else if($gift == 2) new Book()->takeGift();
-    }
-
-    
+    public function takeGift(ITakeGift $giftStrategy): void {
+        $giftStrategy->takeGift();
+    }   
 }
--- index.php
+++ index.php
@@ -1,7 +1,18 @@
 <?php 
-
+/*Note 
+This is clearly a overengineering case. We don't currently need
+a Strategy Pattern. We can discuss when it would be necessary,
+but you can easily solve this exercise with a pair of arrays.
+Sorry, solving it with arrays was boring to me :)
+*/
 include_once 'class/Menu.php';
 
 $menu = new Menu();
 
-$menu->showOptions();
+$choice = $menu->selectOption();
+
+if ($choice === "1") {
+    $menu->takeGift(new Rose());
+} else if ($choice === "2") {
+    $menu->takeGift(new Book());
+}
